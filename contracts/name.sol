6f2f94b105079a6bf44a14c852083df5d4a9d538
Preeti <karnwalpreeti102@gmail.com>
2024-10-18 03:34:06 +0530

    updated interfaces

--- contracts/name.sol
+++ contracts/name.sol
@@ -14,3 +14,21 @@ interface IVerifier {
         uint[3] memory input
     ) external view returns (bool);
 }
+
+interface IOrderVerifier {
+    function verifyProof(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[2] memory input
+    ) external view returns (bool);
+}
+
+interface ISettleVerifier {
+    function verifyProof(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[4] memory input
+    ) external view returns (bool);
+}
