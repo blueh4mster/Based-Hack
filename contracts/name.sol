aa29fdbe4243f2b868563eb34a3b2cb7e5b10714
Preeti <karnwalpreeti102@gmail.com>
2024-10-18 03:49:57 +0530

    init main contract

--- contracts/name.sol
+++ contracts/name.sol
@@ -32,3 +32,32 @@ interface ISettleVerifier {
         uint[4] memory input
     ) external view returns (bool);
 }
+
+contract name {
+    address public WETHAddr;
+    address public DAIAddr;
+    address public matcherAddr;
+    address public depositVerifierAddr;
+    address public withdrawVerifierAddr;
+    address public orderVerifierAddr;
+    address public settleVerifierAddr;
+    address public cancelVerifierAddr;
+    uint[] public zArr;
+    uint[] public nArr;
+    uint[] public oArr;
+    uint[] public mArr; 
+    uint[] public cArr; 
+    uint[] public onArr;
+    uint public settlementPrice;
+
+    constructor(address _WETHAddr, address _DAIAddr, address _matcherAddr, address _depositVerifierAddr, address _withdrawVerifierAddr, address _orderVerifierAddr, address _settleVerifierAddr, address _cancelVerifierAddr) {
+        WETHAddr = _WETHAddr;
+        DAIAddr = _DAIAddr;
+        matcherAddr = _matcherAddr;
+        depositVerifierAddr = _depositVerifierAddr;
+        withdrawVerifierAddr = _withdrawVerifierAddr;
+        orderVerifierAddr = _orderVerifierAddr;
+        settleVerifierAddr = _settleVerifierAddr;
+        cancelVerifierAddr = _cancelVerifierAddr;
+    }
+}
