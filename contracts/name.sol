eb7c62130e7ef5f6d9235e7166740029752a4fce
Preeti <karnwalpreeti102@gmail.com>
2024-10-18 03:32:14 +0530

    added verifier interfaces

---
+++ contracts/name.sol
@@ -0,0 +1,16 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+interface IERC20 {
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+}
+
+interface IVerifier {
+    function verifyProof(
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[3] memory input
+    ) external view returns (bool);
+}
