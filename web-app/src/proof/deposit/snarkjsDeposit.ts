7103e06fd33650bbda934974417a045b82b2b63c
gerceboss <nilavprajapati2004@gmail.com>
2024-10-19 18:02:16 +0530

    added proof generation and refactored it

---
+++ web-app/src/proof/deposit/snarkjsDeposit.ts
@@ -0,0 +1,32 @@
+import { exportCallDataGroth16 } from "../snarkjsProof";
+
+export async function depositCalldata(
+  salt: string,
+  amount: string,
+  asset: string
+) {
+  const input = {
+    salt: salt,
+    amount: amount,
+    asset: asset,
+  };
+
+  try {
+    const dataResult: {
+      a: string[];
+      b: string[][];
+      c: string[];
+      Input: string[];
+    } = await exportCallDataGroth16(
+      input,
+      "/zkproof/deposit.wasm",
+      "/zkproof/deposit_final.zkey"
+    );
+
+    return dataResult;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return null;
+}
