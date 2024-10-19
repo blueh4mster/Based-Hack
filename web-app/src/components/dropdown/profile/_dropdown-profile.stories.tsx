db8ab28a9d014f0ee303e9f2f5e91c6ba4d369da
gerceboss <nilavprajapati2004@gmail.com>
2024-10-20 02:36:51 +0530

    added wallet and provider

--- web-app/src/components/dropdown/profile/_dropdown-profile.stories.tsx
+++ web-app/src/components/dropdown/profile/_dropdown-profile.stories.tsx
@@ -3,7 +3,7 @@ import { Web3Modal } from '@web3modal/react';
 import tw, { styled } from 'twin.macro';
 import { WagmiConfig } from 'wagmi';
 
-import { ethereumClient, projectId, wagmiConfig } from '../../../config/setup-wallet';
+import { ethereumClient, projectId, wagmiConfig } from '../../../config/wallet';
 
 import { DropdownProfile } from '.';
 
---
+++ web-app/src/config/wallet.ts
@@ -0,0 +1,22 @@
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { configureChains, createConfig } from "wagmi";
+import { goerli, baseSepolia } from "wagmi/chains";
+
+import { WALLETCONNECT_PROJECT_ID } from "../constants/constants";
+
+export const chains = [baseSepolia, goerli]; // for test
+export const projectId = WALLETCONNECT_PROJECT_ID;
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+
+export const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+
+export const ethereumClient = new EthereumClient(wagmiConfig, chains);
