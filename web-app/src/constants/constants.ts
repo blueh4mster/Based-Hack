392c6e93ee4c7c7f573047e0f4bca1e5bbdd3e24
gerceboss <nilavprajapati2004@gmail.com>
2024-10-21 00:45:55 +0530

    edited constants

--- web-app/src/constants/constants.ts
+++ web-app/src/constants/constants.ts
@@ -7,7 +7,8 @@ export const DEFAULT_CHAIN_ID = CHAIN_ID.BASE_SEPOLIA; // for testnet
 
 // wallet connect project id
 export const WALLETCONNECT_PROJECT_ID = "";
-
+export const CHAINLINK_ETH_USD = "0x4aDC67696bA383F43DD60A9e78F2C97Fbbfc7cb1"; // on base sepolia
+export const CHAINLINK_DAI_USD = "0xD1092a65338d049DB68D7Be6bD89d17a0929945e"; // on base sepolia
 export const CONTRACT_ADDRESS: Record<string, `0x${string}`> = {
   CHAINLINK_ETH_DAI: "0x773616e4d11a78f511299002da57a0a94577f1f4",
 };
@@ -15,9 +16,9 @@ export const CONTRACT_ADDRESS: Record<string, `0x${string}`> = {
 export const API_URL = "http://localhost:8080";
 // contract address for `veilTrade.sol`
 export const VEILTRADE_CONTRACT_ADDRESS =
-  "0x5d64848D57fa2263827b07E7caC38FC9b1E0B19E"; // exmaple addresses , not meant for the final app
-export const DAI = "0x5d64848D57fa2263827b07E7caC38FC9b1E0B19E";
-export const WETH = "0x83A0C51E2D292b46C38126b6BebF570F49a41FE9";
+  "0x5d64848D57fa2263827b07E7caC38FC9b1E0B19E"; // example addresses , not meant for the final app
+export const DAI = "0x7D90bB8638EED8D8D7624643927FbC9984750360"; // DAI on base sepolia
+export const WETH = "0x999B45BB215209e567FaF486515af43b8353e393"; // WETH on base sepolia
 export const MATCHER = "0xD5D56F27A592F2E9A008647eeAa390cc353dfc36";
 
 export const LOCALSTORAGE_KEYS = {
